7ec9f258357cb18e1fc86f7aa3719724c6262b1a
KalpeshBhatkar <kalpeshbhatkar916@gmail.com>
2019-11-03 16:20:21 +0530

    Add files via upload

---
+++ StudentMaster.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="StudentMaster.aspx.cs" Inherits="StudentMaster" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Full Name : 
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server" />
+                        <asp:HiddenField ID="hdfID" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Mobile No : 
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtMobileNo" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Email : 
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtEmail" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>DOB : 
+                    </td>
+                    <td>
+                        <asp:Calendar ID="calDOB" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Roll No. : 
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtrollno" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Address : 
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtAddress" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Subject List : 
+                    </td>
+                    <td>
+                        <asp:CheckBoxList ID="chkSubject" runat="server" RepeatDirection="Horizontal" />
+
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="lblStatus" runat="server"  />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" >
+                        <asp:Button ID="btnSubmit" runat="server" Text="Insert" OnClick="btnSubmit_Click" />
+                        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <asp:GridView ID="gvStudentMaster" runat="server" AllowPaging="true" PageSize="10" DataKeyNames="StudentID" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:TemplateField HeaderText="Sr.No">
+                        <ItemTemplate>
+                            <%#Container.DataItemIndex+1 %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="MobileNo" HeaderText="MobileNo" />
+                    <asp:BoundField DataField="EmailID" HeaderText="EmailID" />
+                    <asp:BoundField DataField="SDOB" HeaderText="DOB" />
+                    <asp:BoundField DataField="RollNo" HeaderText="RollNo" />
+                    <asp:TemplateField HeaderText="Edit">
+                        <ItemTemplate>
+                            <asp:Button ID="btnEdit" runat="server" Text="Edit" OnClick="btnEdit_Click" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Delete">
+                        <ItemTemplate>
+                            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+                <EmptyDataTemplate>
+                    <label>No Data Found!!!</label>
+                </EmptyDataTemplate>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StudentMaster.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class StudentMaster : System.Web.UI.Page
+{
+    clsStudentMaster obj = null;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Session["UserID"] = 1;
+        if (!IsPostBack)
+        {
+            BindGridview();
+        }
+    }
+
+    public void BindGridview()
+    {
+        obj = new clsStudentMaster();
+        gvStudentMaster.DataSource = obj.SelectStudentMaster(0);
+        gvStudentMaster.DataBind();
+
+        chkSubject.DataSource = obj.SelectSubjectList();
+        chkSubject.DataTextField = "Name";
+        chkSubject.DataValueField = "SubjectID";
+        chkSubject.DataBind();
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        obj = new clsStudentMaster();
+        obj.StudentID = hdfID.Value == string.Empty ? 0 : Convert.ToInt32(hdfID.Value);
+        obj.Name = txtName.Text.Trim();
+        obj.MobileNo = txtMobileNo.Text.Trim();
+        obj.EmailID = txtEmail.Text.Trim();
+        obj.Address = txtAddress.Text.Trim();
+        obj.DOB = calDOB.SelectedDate.Date;
+        obj.RollNo = txtrollno.Text.Trim();
+        obj.UserID = Convert.ToInt32(Session["UserID"]);
+        if (obj.InsertUpdateStudentMaster(obj) > 0)
+        {
+            InsertStudentSubject(obj.StudentID);
+            lblStatus.Text = "Record " + (hdfID.Value == string.Empty ? "Inserted" : "Updated") + " Successfully";
+            Clear();
+            BindGridview();
+        }
+        else
+        {
+            lblStatus.Text = "Opertation Failed!!!";
+        }
+
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        Clear();
+    }
+
+    public void Clear()
+    {
+        hdfID.Value = string.Empty;
+        txtName.Text = string.Empty;
+        txtMobileNo.Text = string.Empty;
+        txtEmail.Text = string.Empty;
+        txtAddress.Text = string.Empty;
+        txtrollno.Text = string.Empty;
+        calDOB.SelectedDate = DateTime.Now.Date;
+        chkSubject.ClearSelection();
+        btnSubmit.Text = "Insert";
+    }
+
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+        GridViewRow gvr = ((Button)sender).Parent.Parent as GridViewRow;
+        int StudentID = Convert.ToInt32(gvStudentMaster.DataKeys[gvr.RowIndex]["StudentID"].ToString());
+        obj = new clsStudentMaster();
+        DataTable dt = obj.SelectStudentMaster(StudentID);
+        if (dt.Rows.Count > 0)
+        {
+            hdfID.Value = dt.Rows[0]["StudentID"].ToString();
+            txtName.Text = dt.Rows[0]["Name"].ToString();
+            txtMobileNo.Text = dt.Rows[0]["MobileNo"].ToString();
+            txtEmail.Text = dt.Rows[0]["EmailID"].ToString();
+            txtAddress.Text = dt.Rows[0]["Address"].ToString();
+            txtrollno.Text = dt.Rows[0]["RollNo"].ToString();
+            calDOB.SelectedDate = Convert.ToDateTime(dt.Rows[0]["SDOB"].ToString());
+            btnSubmit.Text = "Update";
+            chkSubject.ClearSelection();
+            DataTable dt1 = obj.SelectStudentSubjectList(StudentID);
+            if (dt1.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt1.Rows.Count; i++)
+                {
+                    chkSubject.Items.FindByValue(dt1.Rows[i]["SubjectID"].ToString()).Selected = true;
+                }
+            }
+        }
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        GridViewRow gvr = ((Button)sender).Parent.Parent as GridViewRow;
+        int StudentID = Convert.ToInt32(gvStudentMaster.DataKeys[gvr.RowIndex]["StudentID"].ToString());
+        obj = new clsStudentMaster();
+        obj.StudentID = StudentID;
+        obj.UserID = Convert.ToInt32(Session["UserID"]);
+        if (obj.DeleteStudentMaster(obj) > 0)
+        {
+            lblStatus.Text = "Record Deleted Successfully";
+            BindGridview();
+        }
+        else
+        {
+            lblStatus.Text = "Opertation Failed!!!";
+        }
+    }
+
+    public void InsertStudentSubject(int StudentID)
+    {
+        DeleteStudentSubject(StudentID);
+        foreach (ListItem chk in chkSubject.Items)
+        {
+            if (chk.Selected)
+            {
+                obj = new clsStudentMaster();
+                obj.StudentID = StudentID;
+                obj.SubjectID = Convert.ToInt32(chk.Value);
+                obj.InsertStudentSubject(obj);
+            }
+        }
+    }
+
+    public void DeleteStudentSubject(int StudentID)
+    {
+        obj = new clsStudentMaster();
+        obj.DeleteStudentSubject(StudentID);
+    }
+}
